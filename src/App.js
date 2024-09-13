b3b93f21dc99c7f2de8b5bd340972a4c7b112d6c
Nguyễn Thị Quỳnh Mai <147372843+QuynhMai273na@users.noreply.github.com>
2024-09-13 15:35:38 +0700

    Edit app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world project with ReactJS. <br></br>
+          NGUYEN THI QUYNH MAI - 21522321
         </p>
         <a
           className="App-link"
